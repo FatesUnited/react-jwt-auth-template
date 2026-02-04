448f70c4f97926ee97646e596e0d1d227393230f
Thomas Phillips <fatesunited@gmail.com>
2026-02-04 16:27:53 -0600

    finished react jwt auth template lecture

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,43 @@
+// src/components/Dashboard/Dashboard.jsx
+
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../contexts/UserContext';
+import * as userService from '../../services/userService';
+
+const Dashboard = () => {
+  const { user } = useContext(UserContext);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+        try {
+            const fetchedUsers = await userService.index();
+
+            setUsers(fetchedUsers);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    if (user) { fetchUsers(); }
+  }, [user]);
+
+  return (
+    <main>
+      <h1>Welcome, {user.username}</h1>
+      <p>This is the dashboard page where you can see a list of all the users.</p>
+      <ul>
+        {users.map((u) => {
+            return (
+            <li key={u._id}>
+                {u.username}
+            </li>
+            );
+        })}
+      </ul>
+    </main>
+  );
+};
+
+export default Dashboard;
+
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,32 @@
-// src/components/NavBar/NavBar.jsx
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext.jsx"
+import { Link } from "react-router";
 
-const NavBar = () => {
+const Navbar = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    setUser(null);
+  };
+
   return (
     <nav>
-      <p>Navbar contents will go here.</p>
+      {user ? (
+        <ul>
+          <li>Welcome, {user.username}</li>
+          <li><Link to='/'>Dashboard</Link></li>
+          <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
+        </ul>
+      ) : (
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to="/sign-in">Sign In</Link></li>
+          <li><Link to="/sign-up">Sign Up</Link></li>
+        </ul>
+      )}
     </nav>
   );
 };
 
-export default NavBar;
-
+export default Navbar;
